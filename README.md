de2e3b8ec4a4bfe79e165c36aaf4f4a520bd380a
shenyu <shenyu@shenyu.me>
2024-08-13 19:30:24 +0800

    install_dependencies script

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 ## 安装
 
+打开 PowerShell，执行下面这些命令
+
 ```shell
 git clone git@github.com:chrisniael/powershell.git $HOME/.powershell
 
+cd $HOME/.powershell
+
+# 安装依赖
+./install_dependencies.ps1
+
+# 以管理员身份运行
 ./install.ps1
 ```
---
+++ install_dependencies.ps1
@@ -0,0 +1,15 @@
+﻿# posh-git
+# https://github.com/dahlbyk/posh-git
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+
+# PSReadLine
+# https://github.com/PowerShell/PSReadLine
+PowerShellGet\Install-Module PSReadLine -Scope CurrentUser -Force
+
+# Terminal-Icons
+# https://github.com/devblackops/Terminal-Icons
+PowerShellGet\Install-Module Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force
+
+# oh-my-posh 
+# https://github.com/JanDeDobbeleer/oh-my-posh
+winget install JanDeDobbeleer.OhMyPosh -s winget
